e7150679f5a50015eb58f207da9f7e61b566e6a4
Dima Vokov <dima.dima99.vokov@mail.ru>
2020-02-12 03:29:47 +0200

    updating app with a shop page, collection-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -2,32 +2,15 @@ import React from 'react';
 import {Route, Switch, Link} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop'
 
-const TopicsList = props => {
-  return (
-    <div>
-      <Link to={`${props.match.url}/13`}>to 13</Link>
-      <Link to={`${props.match.url}/14`}>to 14</Link>
-      <Link to={`${props.match.url}/16`}>to 16</Link>
-    </div>
-  );
-}
-
-const TopicDetail = props => {
-  return (
-    <div>
-      {props.match.params.topicId}
-    </div>
-  );
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route path='/' component={HomePage} exact={true} />
-        <Route path='/topics' component={TopicsList} exact={true}/>
-        <Route path='/topics/:topicId' component={TopicDetail} />
+        <Route path='/shop' component={ShopPage}/> 
       </Switch>
     </div>
   );

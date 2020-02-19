08d3ccd3cae24156ae8f1533fa6ddb3d11b9b40a
Dima Vokov <dima.dima99.vokov@mail.ru>
2020-02-19 13:30:46 +0200

    updating to redirect to home if user signed in

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 import Header from './components/header/header'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
-
+import {setCurrentUser} from './redux/user/user_actions'
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unSubscribeFromAuth = null
 
   componentDidMount(){
+const {setCurrentUser} = this.props
+
     this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
         
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
+            })
           })
-        })
+        
       }else{
-        this.setState({currentUser: userAuth})
+        setCurrentUser(userAuth)
       }
     })
   }
@@ -45,15 +39,29 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route path='/' component={HomePage} exact={true} />
           <Route path='/shop' component={ShopPage}/> 
-          <Route path='/signin' component={SignInAndSignUpPage}/> 
+          <Route path='/signin' render={() => 
+            this.props.currentUser ? (
+                <Redirect to='/' />
+                ) : (
+                  <SignInAndSignUpPage />
+                )
+              } exact={true}/> 
         </Switch>
       </div>
     )
   }
 }
 
-export default App;
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
